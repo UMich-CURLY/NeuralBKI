4f845b382cefffb589381d414d7169f95d76a95f
Arthur K Zhang <arthurzh@umich.edu>
2022-04-21 17:50:10 -0400

    Tested dataloader with rviz visualizer

---
+++ Data/dataset.py
@@ -0,0 +1,177 @@
+## Maintainer: Arthur Zhang #####
+## Contact: arthurzh@umich.edu #####
+
+import os
+import pdb
+import numpy as np
+import random
+import json
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+from Data.utils import *
+
+class Rellis3dDataset(Dataset):
+    """Rellis3D Dataset for Neural BKI project
+    
+    Access to the processed data, including evaluation labels predictions velodyne poses times
+    """
+    def __init__(self, 
+        directory,
+        device='cuda',
+        num_frames=20,
+        voxelize_input=False,
+        remap=True,
+        use_gt=True,
+        apply_transform=True
+        ):
+        '''Constructor.
+        Parameters:
+            directory: directory to the dataset
+        '''
+
+        self.voxelize_input = voxelize_input
+        self._directory = directory
+        self._num_frames = num_frames
+        self.device = device
+        self.remap = remap
+        self.use_gt = use_gt
+        self.apply_transform = apply_transform
+        
+        self._scenes = sorted(os.listdir(self._directory))
+
+        self._num_scenes = len(self._scenes)
+        self._num_frames_scene = []
+
+        param_file = os.path.join(self._directory, self._scenes[0], 'params.json')
+        with open(param_file) as f:
+            self._eval_param = json.load(f)
+        
+        self._out_dim = self._eval_param['num_channels']
+        self._grid_size = self._eval_param['grid_size']
+        self.grid_dims = np.asarray(self._grid_size)
+        self._eval_size = list(np.uint32(self._grid_size))
+        
+        self.coor_ranges = self._eval_param['min_bound'] + self._eval_param['max_bound']
+        self.voxel_sizes = [abs(self.coor_ranges[3] - self.coor_ranges[0]) / self._grid_size[0], 
+                      abs(self.coor_ranges[4] - self.coor_ranges[1]) / self._grid_size[1],
+                      abs(self.coor_ranges[5] - self.coor_ranges[2]) / self._grid_size[2]]
+        self.min_bound = np.asarray(self.coor_ranges[:3])
+        self.max_bound = np.asarray(self.coor_ranges[3:])
+        self.voxel_sizes = np.asarray(self.voxel_sizes)
+
+        self._velodyne_list = []
+        self._label_list = []
+        self._pred_list = []
+        self._eval_labels = []
+        self._eval_counts = []
+        self._frames_list = []
+        self._timestamps = []
+        self._poses = [] 
+
+        for scene in self._scenes:
+            velodyne_dir = os.path.join(self._directory, scene, 'os1_cloud_node_kitti_bin')
+            label_dir = os.path.join(self._directory, scene, 'os1_cloud_node_semantickitti_label_id')
+            pred_dir = os.path.join(self._directory, scene, 'predictions')
+            
+            self._num_frames_scene.append(len(os.listdir(velodyne_dir)))
+
+            frames_list = [os.path.splitext(filename)[0] for filename in sorted(os.listdir(velodyne_dir))]
+            self._frames_list.extend(frames_list)
+            self._velodyne_list.extend([os.path.join(velodyne_dir, str(frame).zfill(6)+'.bin') for frame in frames_list])
+            self._label_list.extend([os.path.join(label_dir, str(frame).zfill(6)+'.label') for frame in frames_list])
+            self._pred_list.extend([os.path.join(pred_dir, str(frame).zfill(6)+'.bin') \
+                for frame in frames_list])
+            self._poses.append(np.loadtxt(os.path.join(self._directory, scene, 'poses.txt')))
+
+        # Postprocess poses and predictions file to retrieve every fifth pose 
+        self._poses = np.array(self._poses).reshape(-1, 12)
+        
+        self._cum_num_frames = np.cumsum(np.array(self._num_frames_scene) - self._num_frames + 1)
+
+    # Use all frames, if there is no data then zero pad
+    def __len__(self):
+        return sum(self._num_frames_scene)
+    
+    def collate_fn(self, data):
+        output_batch = [bi[0] for bi in data]
+        label_batch = [bi[1] for bi in data]
+        return output_batch, label_batch
+    
+    # def points_to_voxels(self, voxel_grid, points, t_i):
+    #     # Valid voxels (make sure to clip)
+    #     valid_point_mask= np.all(
+    #         (points < self.max_bound) & (points >= self.min_bound), axis=1)
+    #     valid_points = points[valid_point_mask, :]
+    #     voxels = np.floor((valid_points - self.min_bound) / self.voxel_sizes).astype(np.int)
+    #     # Clamp to account for any floating point errors
+    #     maxes = np.reshape(self.grid_dims - 1, (1, 3))
+    #     mins = np.zeros_like(maxes)
+    #     voxels = np.clip(voxels, mins, maxes).astype(np.int)
+    #     # This line is needed to create a mask with number of points, not just binary occupied
+    #     if self.binary_counts:
+    #          voxel_grid[t_i, voxels[:, 0], voxels[:, 1], voxels[:, 2]] += 1
+    #     else:
+    #         unique_voxels, counts = np.unique(voxels, return_counts=True, axis=0)
+    #         unique_voxels = unique_voxels.astype(np.int)
+    #         voxel_grid[t_i, unique_voxels[:, 0], unique_voxels[:, 1], unique_voxels[:, 2]] += counts
+    #     return voxel_grid
+
+    def get_file_path(self, idx):
+        print(self._frames_list[idx])
+    
+    def __getitem__(self, idx):
+        # -1 indicates no data
+        # print("idx ", idx)
+        # the final index is the output
+        idx_range = self.find_horizon(idx)
+         
+        current_points = []
+        current_labels = []
+        
+        curr_pose_mat = self._poses[idx_range[-1]].reshape(3, 4)
+        curr_pose_rot   = curr_pose_mat[0:3, 0:3].T # Global to current rot R^T
+        curr_pose_trans = -curr_pose_rot @ curr_pose_mat[:, 3] # Global to current trans (-R^T * t)
+        for i in idx_range:
+            if i == -1: # Zero pad
+                points = np.zeros((1, 3), dtype=np.float32)
+                labels = np.zeros((1,), dtype=np.uint32)
+            else:
+                points = np.fromfile(self._velodyne_list[i],dtype=np.float32).reshape(-1,4)[:, :3]
+
+                if self.apply_transform:
+                    prev_pose_mat = self._poses[i].reshape(3, 4)
+                    prev_pose_rot = prev_pose_mat[0:3, 0:3]
+                    prev_pose_trans= prev_pose_mat[:, 3]
+                    # pdb.set_trace()
+                    points_in_global = ((prev_pose_rot @ points.T).T + prev_pose_trans)
+                    points = (curr_pose_rot @ points_in_global.T).T + curr_pose_trans
+
+                if not self.use_gt:
+                    preds = np.fromfile(self._pred_list[i], dtype=np.uint32).reshape((-1))
+                else:
+                    preds = np.fromfile(self._label_list[i], dtype=np.uint32).reshape((-1))
+                labels = preds & 0xFFFF
+
+            if self.remap:
+                labels = LABELS_REMAP[labels].astype(np.uint32)
+
+            current_points.append(points)
+            current_labels.append(labels)
+
+        return current_points, current_labels
+    
+    def find_horizon(self, idx):
+        end_idx = idx
+
+        idx_range = np.arange(idx-self._num_frames, idx)+1
+        diffs = np.asarray([int(self._frames_list[end_idx]) - int(self._frames_list[i]) for i in idx_range])
+        good_diffs = -1 * (np.arange(-self._num_frames, 0) + 1)
+        # print("idx range ", idx_range)
+        idx_range[good_diffs != diffs] = -1
+        # print("diffs ", diffs)
+        # print("good diffs ", good_diffs)
+        # print("idx range new ", idx_range)
+        return idx_range
--- Data/utils.py
+++ Data/utils.py
@@ -11,7 +11,7 @@ from visualization_msgs.msg import *
 from geometry_msgs.msg import Point32
 from std_msgs.msg import ColorRGBA
 
-class_remap = np.array([
+LABELS_REMAP = np.array([
     0,  #void
     1,  #dirt
     0,  #none
@@ -43,11 +43,16 @@ class_remap = np.array([
     0,
     0,
     0,
-    18, #puddle
+    17, #puddle
     0,
-    19, #mud
-    20  #rubble
+    18, #mud
+    19  #rubble
 ])
+
+DYNAMIC_LABELS = np.array([
+    7, 12
+])
+
 
 # Classes for RELLIS3d
 # colors = np.array([ # BGR
@@ -74,29 +79,29 @@ class_remap = np.array([
 #     (255, 255, 255) # unknown
 # ]) / 255.0 # normalize each channel [0-1] since is what Open3D uses
 colors = np.array([ # RGB
-    (0,0,0),        #void
-    (20, 64, 108),  #dirt
-    (0,102,0),      #grass
-    (0,255,0),      #tree
-    (153,153,0),    #pole
-    (255,128,0),    #water
-    (255,0,0),      #sky    
-    (0,255,255),    #vehicle
-    (127,0,255),    #object
-    (64,64,64),     #asphalt
-    (0,0,255),      #building
-    (0,0,102),      #log
-    (255,153,204),  #person
-    (204, 0, 102),  #fence
-    (204,153,255),  #bush
-    (170,170,170),  #concrete
-    (255,121,41),   #barrier
-    (239,255,134),  #puddle
-    (34,66,99),     #mud
-    (138,22,110)   #rubble
+    (0,0,0),        #0void
+    (108, 64, 20),  #1dirt
+    (0,102,0),      #2grass
+    (0,255,0),      #3tree
+    (0,153,153),    #4pole
+    (0,128,255),    #5water
+    (0,0,255),      #6sky    
+    (255,255,0),    #7vehicle
+    (255,0,127),    #8object
+    (64,64,64),     #9asphalt
+    (255,0,0),      #10building
+    (102,0,0),      #11log
+    (204,153,255),  #12person
+    (102, 0, 204),  #13fence
+    (255,153,204),  #14bush
+    (170,170,170),  #15concrete
+    (41,121,255),   #16barrier
+    (134,255,239),  #17puddle
+    (99,66,34),     #18mud
+    (110,22,138)   #19rubble
 ]) / 255.0 # normalize each channel [0-1] since is what Open3D uses
 
-def publish_voxels(map, pub, centroids, min_dim, 
+def publish_voxels(map, pub, points, min_dim, 
     max_dim, grid_dims, model="DiscreteBKI", pub_dynamic=False):
     """
     Publishes voxel map over ros to be visualized in rviz
@@ -105,7 +110,7 @@ def publish_voxels(map, pub, centroids, min_dim,
         map: HxWxDxC voxel map where H=height, W=width,
             D=depth, C=num_classes 
         pub: rospy publisher handle
-        centroids: H*W*Dx2 indices from centroids of voxel map
+        points: H*W*Dx2 indices from points of map
         min_dim: 3x1 minimum dimensions in xyz
         max_dim 3x1 maximum dimensions in xyz
         grid_dims: 3x1 voxel grid resolution in xyz
@@ -116,22 +121,17 @@ def publish_voxels(map, pub, centroids, min_dim,
 
     # Only publish nonfree voxels
     nonfree_mask = semantic_map!=0
-    nonfree_centroids = centroids[nonfree_mask]
+    nonfree_points = points[nonfree_mask]
     nonfree_semantic_map = semantic_map[nonfree_mask].reshape(-1, 1)
 
     # Remove dynamic labels if specified
     if not pub_dynamic:
-        dynamic_class = torch.tensor([
-            0,
-            7,
-            8,
-            12
-        ], device=semantic_map.device).reshape(1, -1)
-
+        dynamic_class = torch.tensor(DYNAMIC_LABELS, device=semantic_map.device).reshape(1, -1)
+        
         dynamic_mask = torch.all(
             nonfree_semantic_map.ne(dynamic_class), dim=-1
         )
-        nonfree_centroids = nonfree_centroids[dynamic_mask]
+        nonfree_points = nonfree_points[dynamic_mask]
         nonfree_semantic_map = nonfree_semantic_map[dynamic_mask]
         
 
@@ -165,9 +165,9 @@ def publish_voxels(map, pub, centroids, min_dim,
 
         point = Point32()
         color = ColorRGBA()
-        point.x = nonfree_centroids[i, 0]
-        point.y = nonfree_centroids[i, 1]
-        point.z = nonfree_centroids[i, 2]
+        point.x = nonfree_points[i, 0]
+        point.y = nonfree_points[i, 1]
+        point.z = nonfree_points[i, 2]
 
         color.r, color.g, color.b = colors[pred]
 
@@ -179,17 +179,80 @@ def publish_voxels(map, pub, centroids, min_dim,
 
     pub.publish(next_map)
 
-
-def get_query_neighbors(queries, map):
+def publish_pc(pc, labels, pub, min_dim, 
+    max_dim, grid_dims, model="DiscreteBKI", pub_dynamic=False):
     """
-    Performs 3D convolution in a predefined cubic volume,
-    returning K neighbors to each query point provided.
+    Publishes voxel map over ros to be visualized in rviz
 
     Input:
-        queries: Nx3 set of query points in new point cloud
-        map: HxWxDx
-    Output:
-        
+        map: HxWxDxC voxel map where H=height, W=width,
+            D=depth, C=num_classes 
+        pub: rospy publisher handle
+        points: H*W*Dx2 indices from points of map
+        min_dim: 3x1 minimum dimensions in xyz
+        max_dim 3x1 maximum dimensions in xyz
+        grid_dims: 3x1 voxel grid resolution in xyz
+        model: name of model used (Default: DiscreteBKI)
     """
+
+    # Only publish nonfree voxels
+    nonfree_mask = labels!=0
+    # pdb.set_trace()
+    nonfree_points = pc[nonfree_mask]
+    nonfree_labels = labels[nonfree_mask].reshape(-1, 1)
+
+    # Remove dynamic labels if specified
+    if not pub_dynamic:
+        
+        dynamic_mask = np.logical_not(np.in1d(nonfree_labels, DYNAMIC_LABELS))
+        nonfree_points = nonfree_points[dynamic_mask]
+        nonfree_labels = nonfree_labels[dynamic_mask].reshape(-1)
+        
+
+    next_map = MarkerArray()
+
+    marker = Marker()
+    marker.id = 0
+    marker.ns = model + "semantic_map"
+    marker.header.frame_id = "map" # change this to match model + scene name LMSC_000001
+    marker.type = marker.CUBE_LIST
+    marker.action = marker.ADD
+    
+    # if frame == 0:
+    #     marker.action = marker.ADD
+    # else:
+    #     marker.action = marker.MODIFY
+    marker.lifetime.secs = 0
+    # marker.header.stamp = 0
+
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1
+
+    marker.scale.x = (max_dim[0] - min_dim[0]) / grid_dims[0]
+    marker.scale.y = (max_dim[1] - min_dim[1]) / grid_dims[1]
+    marker.scale.z = (max_dim[2] - min_dim[2]) / grid_dims[2]
+    
+    for i in range(nonfree_labels.shape[0]):              
+        pred = nonfree_labels[i]
+
+        point = Point32()
+        color = ColorRGBA()
+        point.x = nonfree_points[i, 0]
+        point.y = nonfree_points[i, 1]
+        point.z = nonfree_points[i, 2]
+
+        # pdb.set_trace()
+        color.r, color.g, color.b = colors[pred]
+
+        color.a = 1.0
+        marker.points.append(point)
+        marker.colors.append(color)
+    
+    next_map.markers.append(marker)
+
+    pub.publish(next_map)
+
 
 
